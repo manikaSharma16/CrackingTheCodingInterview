f4c67b046cdd702e39743862cfe866ee0b03dc82
manikaSharma16 <99970103+manikaSharma16@users.noreply.github.com>
2023-12-22 14:07:21 +0530

    Update checkPermutation.cpp

--- checkPermutation.cpp
+++ checkPermutation.cpp
@@ -4,14 +4,13 @@
 
 Questions for the interviewer:
 
-1. Which characters are allowed in the strings? : alphabets only
+1. Which characters(character set) are allowed in the strings? : ASCII
 2. What is the maximum length of the strings?
 3. Can the strings be empty?
 4. What is the minimum length of the strings?
 5. Will the strings be of the same size always? : Yes
 6. Are the strings case-sensitive? : Yes
 7. Is white space significant? : Yes
-8. 
 
 For example:
 "God" and "dog": No
@@ -24,4 +23,3 @@ Solution 1: Sort and iterate
 TC: O(nlogn) where n is length of string */
 
 /* Solution2: Hashmap: Check if two strings have identical character count */
-
