6df326bf6f7c5b19178d4e006641047ba9e3ca37
manikaSharma16 <99970103+manikaSharma16@users.noreply.github.com>
2023-12-25 17:17:13 +0530

    Create sortAStack.cpp

---
+++ sortAStack.cpp
@@ -0,0 +1,26 @@
+/*
+
+Write a program to sort a stack such that the smallest items are on the top. 
+You can use an additional temporary stack, but you may not copy the elements into any other data structure (such as an array). 
+The stack supports the following operations: push, pop, peek, and is Empty.
+
+Questions for the interviewer:
+
+1. Are the numbers in stack only int?
+2. What is the maximum length of the stack allowed?
+3. Can the stack be empty?
+4. Can we insert negatives in the stack?
+
+For example:
+stack(bottom to top): 1234 is sorted
+4321 is unsorted
+ */
+
+/*
+Solution 1: 
+TC:  */
+
+/* Solution2:  */
+  
+
+  
